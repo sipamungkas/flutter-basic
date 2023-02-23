789a49f10eed0a13fb7c1b22afbf28582c8d4a86
Ragil Pamungkas <sipamungkas.dev@gmail.com>
2023-02-23 20:11:57 +0700

    feat: add asset config fix error no ascentor, etc
    - fix error no ancestor in materialapp for mediaquery
    - add scroll view
    - add assets / image configuration

--- lib/main.dart
+++ lib/main.dart
@@ -15,23 +15,119 @@ class MyApp extends StatelessWidget {
           appBar: AppBar(
             title: const Text('Flutter Question Task'),
           ),
-          body: const Padding(
-            padding: EdgeInsets.all(16.0),
-            child: QuestionItem(
-              question: 'Where is your home?',
-              category: 'address',
-            ),
+          body: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    const QuestionItem(
+                        question:
+                            'What is Graphic card produced by this vendor?',
+                        category: 'graphic card',
+                        image: 'assets/images/rtx.jpg'),
+                    const SizedBox(height: 8),
+                    const QuestionItem(
+                        question:
+                            'What is Graphic card produced by this vendor?',
+                        category: 'graphic card',
+                        image: 'assets/images/rtx.jpg'),
+                    const SizedBox(height: 8),
+                    const QuestionItem(
+                        question:
+                            'What is Graphic card produced by this vendor?',
+                        category: 'graphic card',
+                        image: 'assets/images/rtx.jpg'),
+                    const SizedBox(height: 8),
+                    const QuestionItem(
+                        question:
+                            'What is Graphic card produced by this vendor?',
+                        category: 'graphic card',
+                        image: 'assets/images/rtx.jpg'),
+                    const SizedBox(height: 8),
+                    const QuestionItem(
+                        question:
+                            'What is Graphic card produced by this vendor?',
+                        category: 'graphic card',
+                        image: 'assets/images/rtx.jpg'),
+                    const SizedBox(height: 8),
+                    const QuestionItem(
+                        question:
+                            'What is Graphic card produced by this vendor?',
+                        category: 'graphic card',
+                        image: 'assets/images/rtx.jpg'),
+                    const SizedBox(height: 8),
+                    const QuestionItem(
+                        question:
+                            'What is Graphic card produced by this vendor?',
+                        category: 'graphic card',
+                        image: 'assets/images/rtx.jpg'),
+                    const SizedBox(height: 8),
+                    const QuestionItem(
+                        question:
+                            'What is Graphic card produced by this vendor?',
+                        category: 'graphic card',
+                        image: 'assets/images/rtx.jpg'),
+                    const SizedBox(height: 8),
+                    const QuestionItem(
+                        question:
+                            'What is Graphic card produced by this vendor?',
+                        category: 'graphic card',
+                        image: 'assets/images/rtx.jpg'),
+                    const SizedBox(height: 60),
+                    const MeduaQueryWidgetExample(),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Builder(builder: (context) {
+                      return Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 100,
+                        color: Colors.green,
+                        alignment: Alignment.center,
+                        child: const Text(
+                            'Media Query in widget, we can not use it without builder, so wrap it with builder'),
+                      );
+                    })
+                  ],
+                ),
+              )),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              debugPrint('tes');
+            },
+            child: const Icon(Icons.new_label),
           ),
         ));
+  }
+}
+
+class MeduaQueryWidgetExample extends StatelessWidget {
+  const MeduaQueryWidgetExample({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 100,
+      color: Colors.green,
+      child: const Text(
+          'Media Query in widget, we can not use it without builder, do export it into a widget'),
+    );
   }
 }
 
 class QuestionItem extends StatelessWidget {
   final String question;
   final String category;
+  final String image;
 
   const QuestionItem(
-      {super.key, required this.question, required this.category});
+      {super.key,
+      required this.question,
+      required this.category,
+      required this.image});
 
   @override
   Widget build(BuildContext context) {
@@ -40,6 +136,16 @@ class QuestionItem extends StatelessWidget {
       mainAxisSize: MainAxisSize.min,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
+        Image.asset(
+          image,
+          height: 150,
+          width: MediaQuery.of(context).size.width,
+          fit: BoxFit.cover,
+          alignment: Alignment.center,
+        ),
+        const SizedBox(
+          height: 8,
+        ),
         Text(
           question,
           style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
